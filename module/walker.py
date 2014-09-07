4ad6795d26aec10a2c78e21f29ad96474ac2d675
Subho-bcrec <subho.prp@gmail.com>
2014-09-07 18:15:50 +0530

    Splitting of files into modules

--- module/walker.py
+++ module/walker.py
@@ -1,11 +1,7 @@
-#!/usr/bin/env python
-
 #module imports
 import os
-from ipdb import set_trace
-import argparse
+#from ipdb import set_trace
 import time
-import sys
 
 
 
@@ -13,7 +9,7 @@ class app:
     def search(self,tag=None,name=None,dir=None,ex=None,show=False):
         time_start=time.time()
         count=0
-        obj=db()
+        
         print "\n\n__________________________________________________________________________"
         print "\nFiletype = %s,\n Filename = %s,\n Search in : %s ,\n exclude path : %s"%(tag,name,dir,ex)
         print "\nSearching......\n\n"
@@ -47,26 +43,3 @@ class app:
 	print "\n----------------------------------------------------------------------------"
         
 	print "\n\nTotal results found: %d, Search-time: %0.3f ns\n\n"%(count,time_end-time_start)
-
-if  __name__=='__main__':
-    parser = argparse.ArgumentParser(description="Searches for a file",epilog="Provide at least -t or -n argument to start with")
-    #parser.add_argument("-w","--web",action="store_true", help="run as flask webapp")
-    group = parser.add_argument_group('group')
-    group.add_argument("-t","--type", help="set file type")
-    group.add_argument("-n","--name", help="search filename")
-    parser.add_argument("-ex","--exclude",nargs='+', help="exclude path patterns")
-    parser.add_argument("-s","--show",action="store_true",help="show files that start with '.'")
-    parser.add_argument("-d","--directory", help="search directory")
-    parser.add_argument("--setup",action="store_true",help="setup database")
-    parser.add_argument("--cleanall",action="store_true",help="cleans database completely")
-    args = parser.parse_args() 
-    
-    
-    if args.type==None and args.name==None:
-        os.system("python app.py -h")
-	sys.exit(0)
-    
-    else :
-        app_obj=app()
-        app_obj.search(args.type,args.name,args.directory,args.exclude,args.show)
-    
---
+++ search.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import argparse
+import sys
+import os
+from module.walker import app
+
+if  __name__=='__main__':
+    parser = argparse.ArgumentParser(description="Searches for a file",epilog="Provide at least -t or -n argument to start with")
+    #parser.add_argument("-w","--web",action="store_true", help="run as flask webapp")
+    group = parser.add_argument_group('group')
+    group.add_argument("-t","--type", help="set file type")
+    group.add_argument("-n","--name", help="search filename")
+    parser.add_argument("-ex","--exclude",nargs='+', help="exclude path patterns")
+    parser.add_argument("-s","--show",action="store_true",help="show files that start with '.'")
+    parser.add_argument("-d","--directory", help="search directory")
+    parser.add_argument("--setup",action="store_true",help="setup database")
+    parser.add_argument("--cleanall",action="store_true",help="cleans database completely")
+    args = parser.parse_args() 
+    
+    
+    if args.type==None and args.name==None:
+        os.system("python app.py -h")
+	sys.exit(0)
+    
+    else :
+        app_obj=app()
+        app_obj.search(args.type,args.name,args.directory,args.exclude,args.show)
+    
