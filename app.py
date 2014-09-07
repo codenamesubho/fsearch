6fd6307fa7ca41c7d85372c15d88c19a428d48d1
Subho-bcrec <subho.prp@gmail.com>
2014-09-07 17:53:30 +0530

    Removed Database feature

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+
+#module imports
 import os
 from ipdb import set_trace
 import argparse
-from db_module.db import db
-#import threading
 import time
+import sys
+
+
+
 class app:
     def search(self,tag=None,name=None,dir=None,ex=None,show=False):
         time_start=time.time()
@@ -44,23 +49,23 @@ class app:
 	print "\n\nTotal results found: %d, Search-time: %0.3f ns\n\n"%(count,time_end-time_start)
 
 if  __name__=='__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-w","--web",action="store_true", help="run as flask webapp")
-    parser.add_argument("-t","--type", help="set file type")
-    parser.add_argument("-n","--name", help="search filename")
+    parser = argparse.ArgumentParser(description="Searches for a file",epilog="Provide at least -t or -n argument to start with")
+    #parser.add_argument("-w","--web",action="store_true", help="run as flask webapp")
+    group = parser.add_argument_group('group')
+    group.add_argument("-t","--type", help="set file type")
+    group.add_argument("-n","--name", help="search filename")
     parser.add_argument("-ex","--exclude",nargs='+', help="exclude path patterns")
     parser.add_argument("-s","--show",action="store_true",help="show files that start with '.'")
     parser.add_argument("-d","--directory", help="search directory")
     parser.add_argument("--setup",action="store_true",help="setup database")
     parser.add_argument("--cleanall",action="store_true",help="cleans database completely")
     args = parser.parse_args() 
-    obj=db()
-    if args.cleanall:
-        obj.cleanall()
-	
-    if args.setup:
-        obj.setup()
-
+    
+    
+    if args.type==None and args.name==None:
+        os.system("python app.py -h")
+	sys.exit(0)
+    
     else :
         app_obj=app()
         app_obj.search(args.type,args.name,args.directory,args.exclude,args.show)
